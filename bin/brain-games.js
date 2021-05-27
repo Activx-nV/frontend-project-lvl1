bb89a1cde56b49e4e4593d3b2f8ac46e985df664
Lauri Kammonen <laurikamm@gmail.com>
2021-05-27 20:52:25 +0300

    add codeclimate,eslint,github actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import askName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+askName();
