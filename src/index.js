ea86c265d3f3d528136d2ffc92b088a11b4fdfc8
Lauri Kammonen <laurikamm@gmail.com>
2021-05-30 14:36:49 +0300

    refactor: output

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const maxCycles = 3;
 const brainGame = (gameCorrectAnswer, description) => {
   console.log('Welcome to the Brain Games!');
   const usersName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${usersName}!\n`);
-  console.log(description, '\n');
+  console.log(`Hello, ${usersName}!`);
+  console.log(description);
   for (let startCycle = 0; startCycle < maxCycles; startCycle += 1) {
     const correctAnswer = gameCorrectAnswer();
     console.log(`Question: ${correctAnswer.question}`);
@@ -15,11 +15,11 @@ const brainGame = (gameCorrectAnswer, description) => {
     if (answer === correctAnswer.answer) {
       console.log('Correct!');
     } else {
-      console.log(`\n${answer} is a wrong answer ;(. Correct answer was ${correctAnswer.answer}.
+      console.log(`'${answer}' is a wrong answer ;(. Correct answer was '${correctAnswer.answer}'.
 Let's play again, ${usersName}!`);
       return;
     }
   }
-  console.log(`\nCongratulations, ${usersName}!`);
+  console.log(`Congratulations, ${usersName}!`);
 };
 export default brainGame;
