5aff55f1693a1d2c5df7acd3e19b8161851d8cd0
lauri <lauri.k@adngames.com>
2022-12-26 10:43:47 +0200

    move getting correct answer in calc game to separate fn

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,21 +4,22 @@ import brainGame from '../index.js';
 const operator = ['+', '-', '*'];
 const questionDescription = 'What is the result of the expression?';
 
+const calcBasicArithmeticOperations = (randomOperator, firstNumber, secondNumber) => {
+  switch (randomOperator) {
+    case '+': return firstNumber + secondNumber;
+    case '-': return firstNumber - secondNumber;
+    case '*': return firstNumber * secondNumber;
+    default: return 'unknown';
+  }
+};
+
 const brainCalc = () => {
   let correctAnswer = 0;
   const firstNumber = getMinToMaxRandomNumber(1, 99);
   const secondNumber = getMinToMaxRandomNumber(1, 99);
   const randomOperator = operator[+getMinToMaxRandomNumber(0, 2)];
 
-  switch (randomOperator) {
-    case '+': correctAnswer = firstNumber + secondNumber;
-      break;
-    case '-': correctAnswer = firstNumber - secondNumber;
-      break;
-    case '*': correctAnswer = firstNumber * secondNumber;
-      break;
-    default: correctAnswer = 'unknown';
-  }
+  correctAnswer = calcBasicArithmeticOperations(randomOperator, firstNumber, secondNumber);
 
   const question = `${firstNumber} ${randomOperator} ${secondNumber}`;
   const answer = String(correctAnswer);
